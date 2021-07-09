1d845732a6aad175e92948701dddaf9693d5806b
EC2 Default User <ec2-user@ip-10-0-0-208.eu-west-1.compute.internal>
2021-07-09 11:32:17 +0000

    Updated the playbook

--- ansible/install_node.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-
-# 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash && chmod 777 . ~/.nvm/nvm.sh && . ~/.nvm/nvm.sh && nvm install node'
